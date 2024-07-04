b73d33a065d9a5ff990d0faba40c56bd50dbd4f1
hppsrc <horahenaripo@gmail.com>
2024-07-03 19:02:17 -0500

    0.2.0 - Added SETLOCAL - Changed format of builds - Added argument checks at run time (-i: install, -fi: forceinstall) - Added a message mentioning if the script did not run as expected and starts the installation process. - When restarting with administrator privileges it verifies if it is a forced installation and directly installs the script version, otherwise it restarts the script as administrator and passes the first argument. - Added check to verify if Dioxide is installed on the system. - Added code to get version and build installed if it exists, otherwise it goes to the common installation process. - In case Dioxide is already installed, it warns if it is a new, old or the same version of the script. - Removed the code in case choice is not installed in the system. - Fixed cases where %DIOXIDE_PATH% has a "\" after its use. - Added a check before adding a value to PATH.

--- Main.bat
+++ Main.bat
@@ -2,17 +2,25 @@
 : Dioxide 
 
 @ECHO OFF && CLS
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 : Variables
-SET VERSION=0.1.0
+SET VERSION=0.2.0
 SET VERSION_STATUS=ALPHA
-SET BUILD=1727
+SET BUILD=70318
 SET PR_TITLE=%CD%
 SET DIOXIDE_PATH=%LOCALAPPDATA%\Hppsrc\Dioxide\
 TITLE Dioxide %VERSION%
 
+: Check args
+IF "%1"=="-i" GOTO :PREINSTALL      @REM Install
+IF "%1"=="-fi" GOTO :PREINSTALL     @REM Force install / Same as force update
+
 : Check path ("Is installed?")
-IF "%~d0%~p0"=="%DIOXIDE_PATH%bin\" ( GOTO PRERUN ) ELSE GOTO PREINSTALL
+IF "%~d0%~p0"=="%DIOXIDE_PATH%bin\" ( GOTO PRERUN ) ELSE (
+    ECHO Detected that this is not a regular Dioxide run, running installation process.
+    GOTO PREINSTALL
+)
 
 :PRERUN
 
@@ -25,12 +33,13 @@ EXIT
 : Get Admin priv
 NET SESSION >nul 2>&1
 IF %errorLevel% == 0 (
-    ECHO.
+    IF "%1"=="-fi" GOTO :CONTINUE_INSTALL     @REM Force install
 ) else (
-    ECHO Not running as administrator, restarting script
+    ECHO Not running as administrator, restarting script.
+    TIMEOUT /T 1 /NOBREAK > NUL
         
     ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\elevate.vbs"
-    ECHO UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\elevate.vbs"
+    ECHO UAC.ShellExecute "%~s0", "%1", "", "runas", 1 >> "%temp%\elevate.vbs"
         
     "%temp%\elevate.vbs"
     DEL "%temp%\elevate.vbs"
@@ -39,10 +48,69 @@ IF %errorLevel% == 0 (
 
 CLS
 
-: Check if CHOICE Exist
-WHERE /q CHOICE && ECHO. || GOTO INSTALL_NC
+: Check if already installed
+IF EXIST %DIOXIDE_PATH%bin\d.bat SET DIOXIDE_CHECK=d.bat
+IF EXIST %DIOXIDE_PATH%bin\di.bat ( SET DIOXIDE_CHECK=di.bat ) ELSE ( GOTO INSTALL)
 
-: Choice Exist
+FOR /f "tokens=2 delims==" %%a IN ('findstr /C:"SET VERSION=" "%DIOXIDE_PATH%bin\%DIOXIDE_CHECK%"') DO (
+    SET INSTALLED_VERSION=%%a
+
+    FOR /f "tokens=2 delims==" %%a IN ('findstr /C:"SET BUILD=" "%DIOXIDE_PATH%bin\%DIOXIDE_CHECK%"') DO (
+        SET INSTALLED_BUILD=%%a
+        GOTO :BREAK
+    )
+
+)
+
+:BREAK
+
+IF DEFINED INSTALLED_VERSION (
+
+    : Script has a new version
+    IF %INSTALLED_BUILD% LSS %BUILD% (
+        ECHO There seems to be an older version of Dioxide installed on the system.
+        ECHO Installed Script:  %INSTALLED_VERSION% ^( %INSTALLED_BUILD% ^)
+        ECHO Current Script:    %VERSION% ^( %BUILD% ^)
+        ECHO.
+        ECHO Do you want to upgrade Dioxide to the version %VERSION%?
+
+        CHOICE /m choose
+
+        echo %errorLevel%
+        pause
+
+        IF %ERRORLEVEL%==1 GOTO CONTINUE_INSTALL
+        IF %ERRORLEVEL%==2 GOTO CLOSE
+    )
+
+    : Script has a old version
+    IF %INSTALLED_BUILD% GTR %BUILD% (
+        ECHO There seems to be a newer version of Dioxide installed on the system.
+        ECHO Installed Script:  %INSTALLED_VERSION% ^( %INSTALLED_BUILD% ^)
+        ECHO Current Script:    %VERSION% ^( %BUILD% ^)
+        ECHO.
+        ECHO Do you want to go back to this previous Dioxide version %VERSION%?
+        ECHO.
+        ECHO Remember that this implies that many functions of the Dioxide may stop working.
+        ECHO If you are installing this because the current version has a bug please report it on github.
+
+        CHOICE /m choose
+
+        IF %ERRORLEVEL%==1 GOTO CONTINUE_INSTALL
+        IF %ERRORLEVEL%==2 GOTO CLOSE
+    )
+
+    : Script has same version
+    IF %INSTALLED_BUILD% EQU %BUILD% (
+        ECHO The installation process was cancelled. 
+        ECHO This script contains the same version as the one installed on the system.
+        ECHO Version installed: %INSTALLED_VERSION% ^( %BUILD% ^)
+        PAUSE
+        GOTO CLOSE
+    )
+)
+
+: Install process
 :INSTALL
 
 ECHO Welcome to the Dioxide installation process
@@ -57,41 +125,25 @@ CHOICE /m choose
 IF %ERRORLEVEL%==1 GOTO CONTINUE_INSTALL
 IF %ERRORLEVEL%==2 GOTO CLOSE
 
-: Choice doesn't exist
-:INSTALL_NC
-
-ECHO Welcome to the Dioxide installation process
-ECHO Dioxide is a script that tries to replicate all the functions of Zoxide in Windows Batch.
-ECHO.
-ECHO You are about to install the version %VERSION% %VERSION_STATUS% (%BUILD%)
-ECHO.
-ECHO Want to continue?
-ECHO [Y/N]?
-
-SET /p ANS=
-
-IF %ANS%==y GOTO CONTINUE_INSTALL
-IF %ANS%==n GOTO CLOSE
-IF %ANS%==Y GOTO CONTINUE_INSTALL
-IF %ANS%==N GOTO CLOSE
-
-CLS
-ECHO ERROR, You entered an invalid value, try again.
-ECHO.
-GOTO INSTALL_NC
-
 : Install Dioxide
 :CONTINUE_INSTALL
 
 CLS
 
 ECHO Creating directories...
-mkdir %DIOXIDE_PATH%\bin\>nul 2>&1
+mkdir %DIOXIDE_PATH%bin\>nul 2>&1
+
 ECHO Creating files...
-COPY %~f0 %DIOXIDE_PATH%\bin\d.bat>nul 2>&1
-COPY %~f0 %DIOXIDE_PATH%\bin\di.bat>nul 2>&1
+COPY %~f0 %DIOXIDE_PATH%bin\d.bat>nul 2>&1
+COPY %~f0 %DIOXIDE_PATH%bin\di.bat>nul 2>&1
+
 ECHO Add to PATH...
-SETX /M PATH "%PATH%;%localappdata%\Hppsrc\Dioxide\bin"
+echo %PATH%>nul | findstr /C:"%DIOXIDE_PATH%bin" >nul
+IF %errorlevel% NEQ 0 (
+    SETX PATH "%PATH%;%DIOXIDE_PATH%bin"
+)
+
+ECHO Creating information files...
 
 ECHO.
 ECHO Dioxide %VERSION% was installed on your system, you can now use it in your terminal.
@@ -100,6 +152,7 @@ EXIT
 
 PAUSE
 
+: Exit script but not close CMD
 :CLOSE
 
 @ECHO ON && TITLE %PR_TITLE% && CLS && 
